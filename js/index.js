c18e20a425e1c89d5d3812076815b74658537c6d
teladesor <ted.rosedale@gmail.com>
2016-02-16 13:25:19 -0500

    final

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Tip: You can either hide the answer on page load with js or use display:none in css
+
+$(document).ready(function(){
+	$("#answer1").hide();
+    $("#question1").click(function(){
+    $("#answer1").slideDown();
+});
+});
+$(document).ready(function(){
+	$("#answer2").hide();
+   	$("#question2").click(function(){
+    $("#answer2").slideDown();
+});
+});
+$(document).ready(function(){
+	$("#answer3").hide();
+    $("#question3").click(function(){
+    $("#answer3").slideDown();
+});
+});
